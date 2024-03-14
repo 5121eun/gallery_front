9eaeed04cfa5e7cfa7f7b7eed54665d7762def1a
5121eun <5121eun@gmail.com>
2024-03-14 15:59:23 +0900

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import TopNav from "./ui/topnav";
+import TopNav from "./ui/nav/topnav";
 
 const inter = Inter({ subsets: ["latin"] });
 
