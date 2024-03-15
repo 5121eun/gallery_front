f73ad79ec9eec8aec7742705e01bfa1eb9ff6c1d
5121eun <5121eun@gmail.com>
2024-03-15 23:05:33 +0900

    update nav

--- app/ui/nav/nav-link.tsx
+++ app/ui/nav/nav-link.tsx
@@ -1,23 +1,28 @@
 "use client"
 
+import { useEffect, useState } from "react";
 import TopLink from "./top-link";
 import { logout } from "@/app/lib/actions"
 
 export default function NavLinks() {
+    const [ login, setLogin ] = useState(false)
+    
     async function rqeuestLogout() {
         logout()
         localStorage.removeItem("login")
         window.location.reload()
     }
 
-    const login = localStorage.getItem("login")
+    useEffect(() => {
+        setLogin(localStorage.getItem("login")!=null)   
+    })
 
     return (
         <div className="flex flex-row">
             <TopLink name="Main" href="/" />
             {
                 // if login show upload and logout, otherwise show login
-                login  != null?
+                login?
                 <>
                     <TopLink name="create" href="/post/create" />
                     <TopLink name="Logout" href="#" onClick={() => rqeuestLogout()} />
