ec3d34c922626142fe9525d7686112dbd5d494f4
5121eun <5121eun@gmail.com>
2024-03-15 16:41:22 +0900

    update chip

--- app/ui/post/chip.tsx
+++ app/ui/post/chip.tsx
@@ -3,7 +3,6 @@ import X from "@/app/ui/svg/x-icon";
 interface ChipProps extends React.HTMLAttributes<HTMLSpanElement> {
     children: string;
     icon?: boolean;
-    color: string;
     handleClick_X?: (evet: React.MouseEvent<HTMLButtonElement>) => void;
     handleClick?: (evet: React.MouseEvent<HTMLButtonElement>) => void;
   }
@@ -12,11 +11,11 @@ export default function Chip({ children, icon, color, handleClick_X, handleClick
     return (
         <span 
         onClick={handleClick}
-        className={`w-fit h-fit cursor-pointer inline-flex items-center px-2 py-1 me-2 text-sm font-medium text-${color}-800 bg-${color}-100 rounded dark:bg-${color}-900 dark:text-${color}-300`}>
+        className="w-fit h-fit cursor-pointer bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
             {children}
             {
                 icon &&
-                <button type="button" className={`items-center p-1 ms-2 text-sm text-${color}-400 bg-transparent rounded-sm hover:bg-${color}-200 hover:text-${color}-900 dark:hover:bg-${color}-800 dark:hover:text-${color}-300`} data-dismiss-target={`#badge-dismiss-${color}`} aria-label="Remove" onClick={handleClick_X}>
+                <button type="button" className="inline-flex items-center p-1 ms-2 text-sm text-gray-400 bg-transparent rounded-sm hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-gray-300" data-dismiss-target={`#badge-dismiss-${color}`} aria-label="Remove" onClick={handleClick_X}>
                     <X />
                 </button>
             }
