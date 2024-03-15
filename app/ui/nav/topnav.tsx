7ddef385ab0cda5185aafd1411bc534cb29a3298
5121eun <5121eun@gmail.com>
2024-03-15 15:13:03 +0900

    update nav

--- app/ui/nav/topnav.tsx
+++ app/ui/nav/topnav.tsx
@@ -1,11 +1,15 @@
 import Logo from "@/app/ui/logo"
 import NavLinks from "./nav-link"
+import DivRow from "../div-row"
 
 export default function TopNav() {
     return (
-        <div className="flex flex-row justify-between items-center p-5 ">
-            <Logo />
-            <NavLinks />
+        <div className="flex flex-col w-full p-10">
+            <div className="flex flex-row justify-between items-end">
+                <Logo />
+                <NavLinks />
+            </div>
+            <hr className="h-0.5 mt-3 bg-gray-200 border-0 dark:bg-gray-700" />
         </div>
     )
 }
